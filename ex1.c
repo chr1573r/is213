0ff3cf9cd535be7d9ffccab90fc5c5beaf2cf1eb
ChristerJ <christer@tetag.no>
2015-09-29 14:57:08 +0200

    git status

--- ex1.c
+++ ex1.c
@@ -1,13 +1,13 @@
-#include <unistd.h>
-#include <stdio.h>
+#include <unistd.h> // header for POSIX API compatibility
+#include <stdio.h> // header for standard system I/O, we need this to do file operations
 
-int main()
+int main() // main program function
 {
-	int ret;
-	ret = truncate ("./pirate.txt", 45);
-	if (ret == -1) {
-		perror ("truncate");
-		return -1;
+	int ret; // integer variable type storing return value from truncate
+	ret = truncate ("./pirate.txt", 45); // attempt to open pirate.txt in current dir, truncate it to 45 bytes
+	if (ret == -1) { // if return is -1, truncate failed
+		perror ("truncate");  // display error "truncate"
+		return -1; // return exitcode -1 to shell to indicate error
 	}
- 	return 0;
+ 	return 0; // return 0 to shell, everything worked
 }
